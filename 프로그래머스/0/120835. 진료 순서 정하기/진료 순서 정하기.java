fdcf749ed439f7793e1ade8196bb785f753af5b3
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-05-16 19:15:55 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0; i<emergency.length; i++) {
             for(int j=0; j<emergency.length; j++) {
                 if(emergency[i]<emergency[j]) {
-                    answer[i]++;
+                    answer[i]++;    //배열에 후위연산자를 사용하면 배열의 해당 인덱스의 값을 증가시킴
                 }
             }
             answer[i]++;
