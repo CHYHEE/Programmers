64660db88c0c1abc203594ef92c151f7d44c3423
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-05-19 21:46:37 +0900

    Update 소인수분해.java

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -6,7 +6,7 @@ class Solution {
         
         for(int i=2; i<=n; i++) {
             while(n%i==0) {
-                if(!answer.contains(i)) {
+                if(!answer.contains(i)) {    // contains: 리스트 안에 있는지 확인
                     answer.add(i);
                 }
                 n /= i;
