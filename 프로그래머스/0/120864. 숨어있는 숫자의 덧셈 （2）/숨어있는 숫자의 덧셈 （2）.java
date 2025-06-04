284c770dfe0fedea00391ef788b0b5018f4b3932
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-06-04 21:30:53 +0900

    Update 숨어있는 숫자의 덧셈 （2）.java

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String my_string) {
         int answer = 0;
         
-        for(String arr : my_string.split("[a-zA-Z]+")) {
+        for(String arr : my_string.split("[a-zA-Z]+")) {    //영문자가 연속된 부분을 기준으로 분리(빈 문자열도 포함됨)
             if(!arr.isEmpty())
                 answer += Integer.parseInt(arr);
         }
