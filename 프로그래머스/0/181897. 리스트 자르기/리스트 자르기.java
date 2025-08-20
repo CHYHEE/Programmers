5ae07d5ab7334cd2155fdba223396c02e4deb237
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-08-20 20:33:05 +0900

    [level 0] Title: 리스트 자르기, Time: 3.19 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -6,19 +6,19 @@ class Solution {
         int b = slicer[1];
         int c = slicer[2];
         if(n == 1) {
-            for(int i =0; i<b+1; i++) {
+            for(int i = 0; i<= b; i++) {
                 list.add(num_list[i]);
             }
         }else if(n == 2) {
             for(int i = a; i<num_list.length; i++) {
                 list.add(num_list[i]);
             }
-        }else if(n ==3) {
-            for(int i = a; i<b+1; i++) {
+        }else if(n == 3) {
+            for(int i = a; i<=b; i++) {
                 list.add(num_list[i]);
             }
         }else if(n == 4) {
-            for(int i = a; i<b+1; i+=c) {
+            for(int i = a; i<=b; i+=c) {
                 list.add(num_list[i]);
 
             }
